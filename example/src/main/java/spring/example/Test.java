80a7d83157a09d09048a332bb14354d9f49cb57a
sys-11 <sys-11@sys-11-PC>
2019-07-23 11:00:56 +0530

    commit

---
+++ example/src/main/java/spring/example/Test.java
@@ -0,0 +1,20 @@
+ package spring.example;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+		Student student=(Student) context.getBean("studentbean");
+	    student.displayInfo();
+		
+	}
+}
